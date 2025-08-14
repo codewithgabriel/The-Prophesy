a71624608220706e84cd34b489497df10ec136a5
codewithgabriel <olatoyeolaide@gmail.com>
2025-08-15 00:48:44 +0100

    update

--- envs/make_env.py
+++ envs/make_env.py
@@ -1,6 +1,8 @@
 from envs.trading_env import TradingEnv
 import pandas as pd
-from envs.config import CONFIG
+from envs.config import CONFIG , TEST_CONFIG
+from stable_baselines3.common.vec_env import DummyVecEnv
+
 
 def make_env(df: pd.DataFrame, train_mode=True):
     """
@@ -28,3 +30,29 @@ def make_env(df: pd.DataFrame, train_mode=True):
             episode_length=CONFIG.get("episode_length", None),
         )
 
+
+def make_eval_env(test_df, window_size=50):
+    """
+    Wrap environment for evaluation to ensure vectorized shape
+    """
+    feature_cols = ["Close", "ret", "sma_10", "sma_50", "rsi_14", "macd", "macd_sig"]
+    return DummyVecEnv([lambda: TradingEnv(
+            df=test_df,
+            feature_cols=feature_cols,
+            window_size=TEST_CONFIG["window_size"],
+            initial_balance=TEST_CONFIG["initial_balance"],
+            commission_pct=TEST_CONFIG["commission_pct"],  
+            commission_fixed=TEST_CONFIG.get("commission_fixed", 0.0),
+            spread_pct=TEST_CONFIG["spread_pct"],
+            slippage_coeff=TEST_CONFIG.get("slippage_coeff", 0.1), # replaces slippage_pct
+            volume_limit=TEST_CONFIG.get("volume_limit", 0.1),
+            max_leverage=TEST_CONFIG["max_leverage"],
+            maintenance_margin=TEST_CONFIG.get("maintenance_margin", 0.25),
+            financing_rate_annual=TEST_CONFIG.get("financing_rate_annual", 0.02),
+            reward_scaling=TEST_CONFIG.get("reward_scaling", 1.0),
+            dd_penalty_coeff=TEST_CONFIG.get("dd_penalty_coeff", 0.0),
+            turnover_penalty_coeff=TEST_CONFIG.get("turnover_penalty_coeff", 0.0),
+            normalize_observations=TEST_CONFIG.get("normalize_observations", True),
+            random_start=TEST_CONFIG.get("random_start", True),
+            episode_length=TEST_CONFIG.get("episode_length", None),
+    )])

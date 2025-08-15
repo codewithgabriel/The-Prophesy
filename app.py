393e4309dd11c79c48eb234b056e9e59cc17d5d3
codewithgabriel <olatoyeolaide@gmail.com>
2025-08-15 01:43:20 +0100

    show data

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ if menu == "Backtest":
         train_df , test_df  = load_and_prepare_data()
         _ , eval_env = create_env(train_df, test_df)
         networth , trades = run_backtest(model, test_df)
-        print(trades)
+        st.write(trades)
         # st.plotly_chart(plot_equity_curve(networth, CONFIG["initial_balance"]), use_container_width=True)
         # st.plotly_chart(plot_trades(test_df, trades), use_container_width=True)
 

197d4897f13717c06145723510d78474807773df
codewithgabriel <olatoyeolaide@gmail.com>
2025-10-04 08:08:22 +0100

    update pandas_ta

--- technical_analysis.py
+++ technical_analysis.py
@@ -2,8 +2,6 @@ import pandas as pd
 import numpy as np
 import lib_ta.pandas_ta as ta
 
-
-
 def add_technical_indicators(df: pd.DataFrame) -> pd.DataFrame:
     """
     Enhances price DataFrame with a rich set of technical indicators.
